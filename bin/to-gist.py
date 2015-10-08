79e277153549340bfb10ff891ffc4d0f34e160b6
John Christopher Jones <john.christopher@alumni.virginia.edu>
2015-10-07 21:58:57 -0400

    Rename to-gist and add draft

--- bin/to-gist.py
+++ bin/to-gist.py
@@ -1,14 +1,1 @@
-#!/usr/bin/env python3
-
-import sys
-
-code = False
-for line in sys.stdin:
-    if line.startswith('```'):
-        if code:
-            print('\n', '// ', line, sep='', end='')
-        else:
-            print('// ', line, '\n', sep='', end='')
-        code = not code
-    else:
-        print('// ' if not code else '', line, sep='', end='')
+/Users/jcj/.dotfiles/bin/to-gist.sh
